09bbc27f85d3ae83e49983509db30b6f5f719a51
yohan9569 <yohan9569@gmail.com>
2021-12-14 21:19:31 +0900

    Develop experiments applying wandb to manage experiments

--- making_models/experiments.py
+++ making_models/experiments.py
@@ -1,3 +1,4 @@
+import itertools
 import multiprocessing
 import os
 import warnings
@@ -12,9 +13,11 @@ from m_m_db import connect
 from m_m_query import *
 from preprocessing import preprocess
 from prophet import Prophet
+from prophet.diagnostics import cross_validation, performance_metrics
 
-sweep_config = {}
+# Define the sweep # sweep_config.yaml
 
+# Initialize the sweep
 sweep_id = wandb.sweep(sweep_config, project="mlops_prophet")
 n_cups = multiprocessing.cpu_count()
 
@@ -27,13 +30,23 @@ data = preprocess(data)
 
 # 실험 함수
 def experiment():
+    with wandb.init() as run:
 
-    metrics = {}
-    wandb.log(metrics)
+        # metrics = {"cv_rmse": }
+        wandb.log(metrics)
 
 
-# 실험 실행
+# 실험 실행 Run the sweep agent
 count = 5
+wandb.login(key="638bebf7fb7de2d84c427979c5808703b7eee6f4")
 wandb.agent(sweep_id, function=experiment, count=count)
 
 # db에 기록?
+
+
+# 3. Initialize a sweep
+# wandb sweep sweep.yaml
+# 4. Launch agent(s)
+# wandb agent your-sweep-id
+# 5. Visualize results
+#
